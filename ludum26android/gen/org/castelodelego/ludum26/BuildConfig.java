4fc8b519db6be21495373bce17308306ff9285e3
Claus Aranha <caranha@gmail.com>
2013-04-26 23:48:09 +0900

    Ludum 26 Project Skeleton

---
+++ ludum26android/gen/org/castelodelego/ludum26/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.castelodelego.ludum26;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ludum26desktop/src/org/castelodelego/ludum26/Main.java
@@ -0,0 +1,16 @@
+package org.castelodelego.ludum26;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "ludum26";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new ludum26entry(), cfg);
+	}
+}
